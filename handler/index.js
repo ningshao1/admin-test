cbdd40f70a538b2c36168f8f285688ec55590baf
ning <1078339678@qq.com>
2018-11-28 14:58:13 +0800

    登录验证功能实现

--- handler/index.js
+++ handler/index.js
@@ -1,11 +1,18 @@
 const common = require("../common");
 const model = require("../model.js");
 exports.index = (req, res) => {
+    if (req.session.userInfo) {
+        res.redirect('/admin/admin');
+        return
+    }
     common.renderTemp(res, "admin/index.html");
 };
 exports.login = (req, res) => {
-    model.users.findUser(
-        { user_name: req.query.name, pwd: req.query.pwd },
+
+    model.users.findUser({
+            user_name: req.query.name,
+            pwd: req.query.pwd
+        },
         (error, data) => {
             if (error) {
                 console.log("查询错误");
@@ -15,10 +22,13 @@ exports.login = (req, res) => {
                 common.res(res, "账号或密码错误", null, 1);
                 return;
             }
+            req.session.userInfo = data;
             common.res(res, "登录成功", null, 0);
         }
     );
 };
 exports.admin = (req, res) => {
-    
+    if (req.session.userInfo)
+        res.send(`管理页面`);
+    else res.redirect('/admin/index')
 };
--- main.js
+++ main.js
@@ -1,14 +1,46 @@
 const express = require("express");
 const config = require("./config");
 const route = require("./route");
+
+const {
+    exec
+} = require("child_process");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect(`mongodb://127.0.0.1:27017/TEST_NAME`);
+const session = require('express-session')
+mongoose.connect(`mongodb://127.0.0.1:27017/TEST_NAME`, {
+    useNewUrlParser: true
+});
 app.engine("html", require("ejs").renderFile);
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+app.use(session({ //传入中间件函数，参数是一个对象
+    secret: '123456',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 1000 * 60,
+        httpOnly: true
+    },
+    rolling: true
+}));
 app.use(express.static("static"));
 app.use(route);
+
 const server = app.listen(config.cfg.port, "127.0.0.1", () => {
-    console.log(`http://${server.address().address}:${server.address().port}`);
+    const url = `http://${server.address().address}:${server.address().port}/admin/index`
+    switch (process.platform) {
+        //mac系统使用 一下命令打开url在浏览器
+        case "darwin":
+            exec(`open ${url}`);
+            break;
+            //win系统使用 一下命令打开url在浏览器
+        case "win32":
+            exec(`start ${url}`);
+            break;
+            // 默认win系统
+        default:
+            exec(`start ${url}`);
+    }
 });
