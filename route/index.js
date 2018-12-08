86c6dd0fb84acb5fc7f21a2fe1353670cae86fce
ning <1078339678@qq.com>
2018-12-08 15:19:04 +0800

    添加pm2配置

--- route/index.js
+++ route/index.js
@@ -2,26 +2,33 @@ const express = require("express");
 const router = express.Router();
 const handler = require("../handler");
 router.all(`*`, (req, res, next) => {
-    res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader("Cache-Control", "no-cache");
 
-    if (!req.session.userInfo && req.path !== '/admin/index' && req.path !== `/admin/ajax_login`) {
-        res.redirect('/admin/index');
-    }
-    else {
-        next();
-    }
-})
+  if (
+    !req.session.userInfo &&
+    req.path !== "/admin/index" &&
+    req.path !== `/admin/ajax_login`
+  ) {
+    res.redirect("/admin/index");
+  } else {
+    next();
+  }
+});
 router.get("/admin/index", handler.index);
 router.get(`/admin/ajax_login`, handler.login);
 router.get(`/admin/admin`, handler.admin);
-router.get(`/admin/ajax_menu_list`, handler.menuList)
+router.get(`/admin/ajax_menu_list`, handler.menuList);
 router.get(`/admin/ajax_dataclass_list`, handler.dataClassList);
 router.get(`/admin/ajax_logout`, handler.logout);
 router.get(`/admin/ajax_dataclass_add`, handler.dataClassAdd);
-router.get(`/admin/ajax_dataclass_get`, handler.dataClassGet)
+router.get(`/admin/ajax_dataclass_get`, handler.dataClassGet);
 router.get(`/admin/ajax_data_list`, handler.dataListGet);
 router.get(`/admin/ajax_data_get`, handler.dataListRowHandler);
 router.post(`/admin/ajax_data_add`, handler.dataListAdd);
 router.get(`/admin/ajax_art_single_get`, handler.single);
-router.get(`/admin/ajax_art_single_update`, handler.singleUpdate)
+router.post(`/admin/ajax_art_single_update`, handler.singleUpdate);
+router.get(`/admin/ajax_admin_updatepwd`, handler.updatepwd);
+router.get(`/admin/ajax_admin_list`, handler.adminList);
+router.get(`/admin/ajax_admin_del`, handler.adminDel);
+router.get(`/admin/ajax_admin_add`, handler.adminAdd);
 module.exports = router;
--- temp-use.js
+++ temp-use.js
@@ -1,15 +1,18 @@
 const axios = require("axios");
 const mongoose = require("mongoose");
 mongoose.connect(
-  `mongodb://106.14.124.207:27017/TEST_NAME`, {
+  `mongodb://106.14.124.207:27017/TEST_NAME`,
+  {
     useNewUrlParser: true
   }
 );
 axios
   .get(
-    `http://localhost:8000/admin/ajax_data_list?type=1&random=0.519396737915049`, {
+    `http://127.0.0.1:8000/admin/ajax_admin_list?random=0.7450246911556624`,
+    {
       headers: {
-        Cookie: "express:sess=eyJzZXNzX2FkbWluIjp7Im5hbWUiOiJhZG1pbiIsInB3ZCI6IjIxMjMyZjI5N2E1N2E1YTc0Mzg5NGEwZTRhODAxZmMzIiwiYWRkX3RpbWUiOjB9fQ==; express:sess.sig=PFjVLRomrWDIpJ859gIZm9HN2LI"
+        Cookie:
+          "connect.sid=s%3AY4RhMrKAiWWy-zJh9jEeadEK52pN9-XH.6iaPVqYe0lltN8b4U0kdg20S8ZSKwK%2FQcWrgyPABQSM; express:sess=eyJzZXNzX2FkbWluIjp7Im5hbWUiOiJhZG1pbiIsInB3ZCI6IjIxMjMyZjI5N2E1N2E1YTc0Mzg5NGEwZTRhODAxZmMzIiwiYWRkX3RpbWUiOjB9fQ==; express:sess.sig=PFjVLRomrWDIpJ859gIZm9HN2LI"
       }
     }
   )
@@ -20,22 +23,21 @@ axios
     });
     mongoose
       .model(
-        "data",
-        new mongoose.Schema({
-          add_time: Date,
-          content: String,
-          dataclass_id: Number,
-          hits: Number,
-          id: Number,
-          name: String,
-          picture: String,
-          sort: Number,
-          type: Number
-        }, {
-          versionKey: false,
-          autoIndex: true
-        })
-      ).insertMany(data.data.data.list);
+        "user",
+        new mongoose.Schema(
+          {
+            add_time: Date,
+            id: Number,
+            name: String,
+            pwd: String
+          },
+          {
+            versionKey: false,
+            autoIndex: true
+          }
+        )
+      )
+      .insertMany(data.data.data.list);
 
     //var Tank = Mongoose.model('Tank', yourSchema);var small = new Tank({ size: 'small' });
   });
